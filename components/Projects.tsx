0ea5dbd42eb9fa003a0868f6888e19f3d058126e
Dhritabrata <dhritabratamitra@gmail.com>
2025-07-17 14:03:27 -0400

    Added my sixth project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
         <h2 className="h2 fade-in">My Projects</h2>
         <div className="grid grid-cols-2 gap-4 h-full fade-in">
             {projects.map(projects => (
-                <div key={projects.id} className="flex flex-col items-center p-2 mt-2 text-center scaleProject">
+                <div key={projects.id} className="flex flex-col items-center p-2 mt-2 text-center">
                     <iframe className="w-full h-[315px] rounded-[10px] aspect-video" src={projects.videoUrl} title={projects.title} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                     <span className="title">{projects.title}</span>
                     <div className='min-h-[300px]'>
@@ -16,8 +16,8 @@ const Projects = () => {
                     </div>
                     {/* <div className='w-full p-2 mt-auto flex flex-row gap-6 text-center items-center'> */}
                     <div className='w-full p-2 mt-auto grid grid-cols-2 gap-6 text-center items-center'>
-                      {projects.GitHub?<span className='mt-7 w-2'><a href={projects.GitHub} target="_blank" rel="noopener noreferrer"> <FaGithub size={60} /></a></span>:''}
-                      {projects.Link?<button className="mt-7 text-2xl border-corners hover:cursor-pointer hover:bg-[#e0e1dd] transition-colors duration-400"><a href={projects.Link} target="_blank" rel="noopener noreferrer">Click to try</a></button>:''}
+                      {projects.GitHub?<span className='mt-7 w-2 scaleProject'><a href={projects.GitHub} target="_blank" rel="noopener noreferrer"> <FaGithub size={60} /></a></span>:''}
+                      {projects.Link?<button className="scaleProject mt-7 text-2xl border-corners hover:cursor-pointer hover:bg-[#e0e1dd] transition-colors duration-400"><a href={projects.Link} target="_blank" rel="noopener noreferrer">Click to try</a></button>:''}
                     </div>
                 </div>
             ))}
