197c582044fd18e76821fffb449638efd05b9803
Dhritabrata <dhritabratamitra@gmail.com>
2025-08-08 10:52:15 -0700

    Added change in About.tsx component to include educational background.

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="center-align fade-in">
                 <h2 className="h2 fade-in">About Me</h2>
-                <p className="pt-5 fade-in">A Full Stack Developer with a Master&apos;s degree in Computer Science.  
+                <p className="pt-5 fade-in">A Full Stack Engineer with a Master&apos;s degree in Computer Science from Michigan Technological University.  
             I specialize in building modern web applications. 
             With a strong foundation in both frontend and backend development, I focus on creating efficient,  
             scalable, and user-friendly solutions. I am always eager to learn new technologies and solve complex problems through code.</p>
